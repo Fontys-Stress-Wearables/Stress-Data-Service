01b53e93db21ea578f79ecb5a43ae8329830bb78
J.J.C. van Nuland <jorickvannuland@hotmail.com>
2022-04-21 11:10:25 +0200

    - Changed Natsmessage to accurately report origin

--- StressDataService/Nats/NatsMessage.cs
+++ StressDataService/Nats/NatsMessage.cs
@@ -1,6 +1,6 @@
 public class NatsMessage<T>
 {
-    public string origin { get; set; } = "organization-service";
+    public string origin { get; set; } = "Stress Data Service";
     public string target { get; set; }
     public T message { get; set; }
 }
