8bc5a4261da5dfebc3de447d37b7d80ab893bd4c
J.J.C. van Nuland <jorickvannuland@hotmail.com>
2022-04-21 12:02:51 +0200

    - Renamed origin in Natsmessage

--- StressDataService/Nats/NatsMessage.cs
+++ StressDataService/Nats/NatsMessage.cs
@@ -1,6 +1,6 @@
 public class NatsMessage<T>
 {
-    public string origin { get; set; } = "Stress Data Service";
+    public string origin { get; set; } = "stress_data_service";
     public string target { get; set; }
     public T message { get; set; }
 }
