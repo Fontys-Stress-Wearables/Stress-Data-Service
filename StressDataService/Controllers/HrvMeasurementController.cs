3663b45e4051c9940148253fad5d261a6023dc17
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-11-07 12:15:33 +0100

    update consistent naming of private variables in HrvMeasurementController.cs

--- StressDataService/Controllers/HrvMeasurementController.cs
+++ StressDataService/Controllers/HrvMeasurementController.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace StressDataService.Controllers
 {
     [Route("[controller]")]
